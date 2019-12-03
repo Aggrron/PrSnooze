171166302ceddf1ed4d005bbeb61182d1011b016
Aggrron <gsm10forhl2@yandex.ru>
2019-12-03 14:27:48 +0300

    Basic Slingshot Movement

---
+++ Snooze/Snooze/Assets/Scripts/SnoozeControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SnoozeControl : MonoBehaviour {
+	
+	public Rigidbody2D rb;
+
+	private Vector2 st_pos;
+	private Vector2 next_pos;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+		
+	void OnMouseDown(){
+		st_pos = Input.mousePosition;
+	}
+	void OnMouseUp(){
+		next_pos = Input.mousePosition;
+		rb.AddForce (st_pos - next_pos);
+		//Debug.Log(next_pos);
+	}
+}
